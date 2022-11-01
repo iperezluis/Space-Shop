d124584e4ceb699aad98cc258a56eb51e2d39de5
iperezluis <perezvluisv@gmail.com>
2022-11-01 12:51:08 -0400

    updating docker-compose

--- database/db.ts
+++ database/db.ts
@@ -27,12 +27,7 @@ export const connect = async () => {
     await mongoose.disconnect();
   }
   try {
-    await mongoose.connect(process.env.MONGO_URL || "", {
-      // useNewUrlParser: true,
-      // useUnifiedTopology: true,
-      // serverSelectionTimeoutMS: 5000,
-      // family: 4,
-    });
+    await mongoose.connect(process.env.MONGO_URL || "");
     mongoConnection.isConnected = 1;
     console.log("Conectado a MongoDB:", process.env.MONGO_URL);
   } catch (error: any) {
