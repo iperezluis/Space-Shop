acb207d27431323b5d659629bdb32aee30dab46a
Luis Perez <perezvluisv@gmail.com>
2022-12-16 03:09:27 -0400

    fixing env

--- next.config.js
+++ next.config.js
@@ -10,8 +10,15 @@ const nextConfig = {
     esmExternals: false,
   },
   env: {
-    MONGO_URL: `mongodb+svr://${process.env.DB_USER}:${DB_PASS}${DB_HOST}`,
-    PORT: "3000",
+    MONGO_URL:
+      "mongodb+srv://space_shop:auth9**@backend-cafe.y7fo5.mongodb.net/spacedb",
+    PORT: 3000,
+    SECRET_KEY_JWT: "my_secret33@@",
+    NEXTAUTH_SECRET: "mynextauth123@",
+    NEXT_PUBLIC_TAX_RATE: 0.15,
+    DEFAULT_LIMIT: 5,
+    GITHUB_ID: "be8044fe723fed45df1c",
+    GITHUB_SECRET: "8d2dfec911e576215c0fb86993670bfe301a3346",
   },
 };
 
